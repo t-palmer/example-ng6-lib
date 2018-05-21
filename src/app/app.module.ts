d06370f8fb74e59f44bb08fb56585ede77a4df1f
Todd Palmer <tpalmer@us.fujitsu.com>
2018-05-20 21:12:45 -0400

    use a component from the library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExampleNg6LibModule } from 'example-ng6-lib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ExampleNg6LibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
